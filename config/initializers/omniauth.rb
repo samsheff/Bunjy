7264d171f43903e76a9c8d51195c5ea745b88fef
samsheff <ssheffres@gmail.com>
2014-05-30 20:22:13 -0400

    Fix omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "292495877591711", "f36677aa777a903962b8ca62997b36f1"
+  provider :facebook, "292495877591711", "f36677aa777a903962b8ca62997b36f1" if !Rails.env.production?
   provider :facebook, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'] if Rails.env.production?
 end
