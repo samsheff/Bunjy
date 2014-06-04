cd9ad36617bd8c2dab2ca7e205794b76fe25d2f8
Sam Sheffres <ssheffres@gmail.com>
2014-06-04 17:33:44 -0400

    Changed production logger level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Bunjy::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :error
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
