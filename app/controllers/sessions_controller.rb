1af2fea29faa79a1702b176bdebcd6d3a249dc37
Sam Sheffres <ssheffres@gmail.com>
2014-06-05 11:37:28 -0400

    Fix current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,7 +28,7 @@ class SessionsController < ApplicationController
       end
     else
       if @identity.user
-        current_user = @identity.user
+        session[:user_id] = @identity.user.id
         redirect_to user_path(current_user), notice: "Signed in!"
       else
         redirect_to root_url, notice: "There was an error signing you in"
